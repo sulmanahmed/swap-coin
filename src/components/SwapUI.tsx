a74c092ab6cd05875507d925445d22d8c7cc36c1
Sulman Ahmed <sulman.ahmed@codora.io>
2024-11-02 19:46:50 +0500

    updated commit

--- src/components/SwapUI.tsx
+++ src/components/SwapUI.tsx
@@ -38,10 +38,12 @@ const SwapUI: React.FC = () => {
 
 const connectWallet = async () => {
   const wallets = await onboard.connectWallet();
-  connectedWallet(wallets[0]);
+  setConnectedWallet(wallets[0]);
   if (wallets[0]) {
     const ethersProvider = new ethers.BrowserProvider(wallets[0].provider, 'any')
     const signer = ethersProvider.getSigner();
+    console.log('Connected wallet:', wallets[0]);
+    console.log('Signer:', signer);
   }
 };
 
